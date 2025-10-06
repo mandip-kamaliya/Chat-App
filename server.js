3c76a65d3d590f4d08032516eeb2d405e566a83b
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-10-06 17:39:57 +0530

    installed nodemon

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import {express} from "express";
+import express from "express";
 import { configDotenv } from "dotenv";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.listen(PORT,()=>{
