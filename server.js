b7d6075dbdce7288309c591b2c55738bc5c8884e
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-10-07 11:58:41 +0530

    added static file index.html to our server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { configDotenv } from "dotenv";
-import path from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-
-app.get("/Chat-App",(req,res)=>{
-    res.sendFile(__dirname,"public","index.html");
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"public","index.html"));
 })
 
 app.listen(PORT,()=>{
