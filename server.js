d094f92f9e305aee1166ef4eee7f9662ab5dbc6c
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-10-07 11:53:59 +0530

    added path

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+
+
+app.get("/Chat-App",(req,res)=>{
+    res.sendFile(__dirname,"public","index.html");
+})
 
 app.listen(PORT,()=>{
     console.log(`App is listening at port ${PORT}`);
