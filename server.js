06ec30836839c482802700c7ae3b5f9b9b11754c
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-10-08 15:26:59 +0530

    add predefined disconnect event

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ io.on("connection",(socket)=>{
     socket.on("chat message",(msg)=>{
         console.log("message",msg);
         io.emit("chat message",msg);
+    });
+
+    socket.on("disconnect",()=>{
+        console.log("user disconnected");
     })
 })
 
